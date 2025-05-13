bff47ebc3d964e30f3494b53cd840b2a32ecf01d
Gavin <kirengaboris07@gmail.com>
2025-05-13 22:42:32 +0200

    Creation of Routes and Migrations

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\TaskController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/',[TaskController::class, 'index']);
+Route::post('/tasks',[TaskController::class, 'store']);
+Route::put('/tasks/{task}',[TaskController::class, 'update']);
+Route::get('/tasks/{task}',[TaskController::class, 'destroy']);
